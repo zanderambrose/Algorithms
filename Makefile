2695bec4f9112f4003694781e894d256d025afdb
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-28 16:51:17 -0400

    First commit.

---
+++ Makefile
@@ -0,0 +1,3 @@
+ss:
+	g++ --std=c++17 -o _.selectionSort selectionSort.cpp;
+	./_.selectionSort;
---
+++ selectionSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+	int myArr[] = {3, 22, 5, 6, 4, 3, 7, 8, 6, 9};
+	int size = sizeof(myArr) / sizeof(myArr[0]);
+
+	for (int i = 0; i < size; i++)
+	{
+		int minIndex = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (myArr[j] < myArr[minIndex])
+			{
+				minIndex = j;
+			}
+		}
+		int temp = myArr[i];
+		myArr[i] = myArr[minIndex];
+		myArr[minIndex] = temp;
+	}
+
+	for (auto num : myArr)
+	{
+		std::cout << num << ", ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
