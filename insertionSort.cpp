517654f50fe987d46f07eec85e2bf8631af22741
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-28 17:07:15 -0400

    Implement insertion sort.

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -5,6 +5,18 @@ int main()
 	int myArr[] = {3, 22, 5, 6, 4, 3, 7, 8, 6, 9};
 	int size = sizeof(myArr) / sizeof(myArr[0]);
 
+	for (int i = 1; i < size; i++)
+	{
+		int temp = myArr[i];
+		int j = i - 1;
+		while (j > -1 && temp < myArr[j])
+		{
+			myArr[j + 1] = myArr[j];
+			myArr[j] = temp;
+			j--;
+		}
+	}
+
 	for (auto num : myArr)
 	{
 		std::cout << num << ", ";
